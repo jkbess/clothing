86bdaa73f2b72d8ce2dd7ad6b4539c613499f89c
Bess <BessJ@HillsboroughCounty.ORG>
2020-06-15 12:19:21 -0400

    Replaced some component functions with selectors using reselect.

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,16 @@
+import { createSelector } from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [ selectCart ],
+  cart => cart.cartIems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems  => cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  )
+);
